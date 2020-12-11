823b02ebe9d4acdcbe56b0cb4b306420ff79d65c
biingen <biingen@gmail.com>
2020-12-11 08:08:20 +0800

    Remove the main function code.

--- FPD/FPD/Program.cs
+++ FPD/FPD/Program.cs
@@ -12,21 +12,11 @@ namespace FPD
         /// 應用程式的主要進入點。
         /// </summary>
         [STAThread]
-        static void Main(string[] args)
+        static void Main()
         {
-            if (args.Length == 0)
-            {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new Form_Main());
-            }
-            else
-            {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new Form_Main(args[0].ToString()));
-            }
-
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form_Main());
         }
     }
 }
